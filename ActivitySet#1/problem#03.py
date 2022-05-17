f5e712095862278bd59c29b7c6be7c858b87415e
EZYCJ <chiragkumar2224@gmail.com>
2022-05-17 12:44:02 +0530

    done

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,3 +1,5 @@
 # Variables, Expressions & Statements
 
 hrs = float(input("Enter hours? "))
+pay=hrs*2.75
+print(pay)
--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,11 @@
 # Conditional Execution
 
 hrs = input("Enter hours? ")
+rate = input("enter hourly rate ")
+h=float(hrs)
+r=float(rate)
+if h<40:
+ pay=r*h
+if h>40:
+ pay=1.5*r*h
+print(pay)
